450620c19f0845160df4252480825314d73f26b1
Artem <artikboltut@gmail.com>
2025-12-16 20:05:28 +0300

    задание 3.cpp

--- задание 3.cpp
+++ задание 3.cpp
@@ -1,9 +1,11 @@
-﻿#include <iostream>
 #include <algorithm>
+#include <iostream>
+#include <climits>
+#include <vector>
 #include <cstdlib>
 bool is_correct(int);
 int number_positive(int, int*);
-void enter_elements(int, int*);
+void enter_elements(int, int*,int,int);
 int last_zero(int, int*);
 int sum_after_zero(int, int*);
 int max_negative(int, int*);
@@ -14,20 +16,26 @@ void output_zero(int, int*);
 int find_negative(int, int, int*);
 int min_negative(int, int*);
 int min_el_array(int, int*);
-int output_positive(int, int*);
+std::vector <int> find_positive(int, int*);
 void array_sort(int, int*);
-int output_negative(int, int*);
+std::vector <int> find_negative(int, int*);
+void output_positive(int, int*);
+void output_negative(int, int*);
 int main()
 {
-	const int max_size = 32767;
+	const int max_size = INT16_MAX;
 	int n = 0;
 	int* arr = new int[max_size];
 	std::cout << "enter the number of array elements\n";
 	std::cin >> n;
+	std::cout << "Enter random";
+	int32_t a = 0;
+	int32_t b = 0;
+	std::cin >> a >> b;
 	if (is_correct(n) == 0) {
 		std::cout << "error,wrong number"; return 0;
 	}
-	enter_elements(n, arr);
+	enter_elements(n, arr,a,b);
 	std::cout << "the number of positive elements" << " " << number_positive(n, arr) << std::endl;
 	if (last_zero(n, arr) == -1) {
 		std::cout << "no zero in array" << std::endl;
@@ -41,14 +49,13 @@ int main()
 	output_negative(n, arr);
 	delete[] arr;
 }
-void enter_elements(int n, int* arr) {
+void enter_elements(int n, int* arr,int a,int b) {
 	for (int i = 0; i < n; i++) {
-		arr[i] =-10000 + rand() % (10000 - -10000) ;
-		std::cout << arr[i] << " ";
+		arr[i] = a + rand() % (a - b);
 	}
 }
 bool is_correct(int n) {
-	if (n <= 0 || n > 32767) {
+	if (n <= 0 || n > INT16_MAX) {
 		return 0;
 	}
 	else return 1;
@@ -79,7 +86,7 @@ int sum_after_zero(int n, int* arr) {
 	return sum;
 }
 int max_negative(int n, int* arr) {
-	int max_neg = -100000000000;
+	int max_neg = -1 * INT16_MAX;
 	for (int i = 0; i < n; i++) {
 		if (arr[i] < 0 && arr[i] > max_neg) {
 			max_neg = arr[i];
@@ -156,23 +163,35 @@ void array_sort(int n, int* arr) {
 		}
 	}
 }
-int output_positive(int n, int* arr) {
-	if (amount_positive(n, arr) == 0) {
-		return 0;
-	}
-	for (int i = 0; i < n; i++) {
+std::vector <int> find_positive(int n, int* arr) {
+	std::vector <int> result;
+	for (size_t i = 0; i < n; i++) {
 		if (arr[i] > 0) {
-			std::cout << arr[i] << " ";
+			result.push_back(arr[i]);
 		}
+	}
+	return result;
+}
+std::vector <int> find_negative(int n, int* arr) {
+	std::vector <int> result;
+	for (size_t i = 0; i < n; i++) {
+		if (arr[i] < 0) {
+			result.push_back(arr[i]);
+		}
+	}
+	return result;
+}
+void output_positive(int n, int* arr) {
+	std::vector <int> temp = find_positive(n, arr);
+	int32_t size = temp.size();
+	for (size_t i = 0; i < size; i++) {
+		std::cout << temp[i] << " ";
 	}
 }
-int output_negative(int n, int* arr) {
-	if (amount_negative(n, arr) == 0) {
-		return 0;
-	}
-	for (int i = n - 1; i >= 0; i--) {
-		if (arr[i] < 0) {
-			std::cout << arr[i] << " ";
-		}
+void output_negative(int n, int* arr) {
+	std::vector <int> temp = find_negative(n, arr);
+	int32_t size = temp.size();
+	for (int32_t i = size - 1; i >= 0; i--) {
+		std::cout << temp[i] << " ";
 	}
 }
